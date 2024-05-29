3f696a1bf420ca2b9da0845eca26c67bc3e3c43e
ALi <ahamed.aaa01@gmail.com>
2024-05-29 06:04:36 +0530

    Quiz app v1.0.0
    Quiz app made.

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        "question": "Which language runs in a web browser",
+        "options": ["Java", "C", "Python", "JavaScript"],
+        "answer": 4
+    },
+    {
+        "question": "What does CSS stand for",
+        "options": ["Central Style Sheets", "Cascading Style Sheets", "Cool Style Sheets", "Create Style Sheets"],
+        "answer": 2
+    },
+    {
+        "question": "What is the correct HTML element for the largest heading",
+        "options": ["<heading>", "<h1>", "<h6>", "<head>"],
+        "answer": 2
+    },
+    {
+        "question": "What is the default value of the position property in CSS",
+        "options": ["relative", "absolute", "fixed", "static"],
+        "answer": 4
+    },
+    {
+        "question": "Which company developed the React library",
+        "options": ["Google", "Facebook", "Microsoft", "Apple"],
+        "answer": 2
+    }
+]
